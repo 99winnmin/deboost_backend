2c9af7eb96efa0482711faa1ea905e7705e2c8a2
99winnmin <ojysep9987@gmail.com>
2023-05-11 18:36:46 +0900

    [Feat] DE:BOOST Project Init Setting

---
+++ src/main/java/com/samnamja/deboost/DeboostApplication.java
@@ -0,0 +1,13 @@
+package com.samnamja.deboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeboostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeboostApplication.class, args);
+    }
+
+}
