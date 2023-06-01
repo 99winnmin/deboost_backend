3234375f50505fc3a3c63d1fa339c7c0b342ebe1
99winnmin <ojysep9987@gmail.com>
2023-06-01 21:53:26 +0900

    [Feat] 전적 검색, 상세 검색, 전적 데이터 업데이트 비지니스 코드

--- src/main/java/com/samnamja/deboost/api/service/riot/RiotDataService.java
+++ src/main/java/com/samnamja/deboost/api/service/riot/RiotDataService.java
@@ -2,7 +2,12 @@ package com.samnamja.deboost.api.service.riot;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.samnamja.deboost.api.dto.openfeign.response.*;
+import com.samnamja.deboost.api.dto.openfeign.response.GameAllDetailInfoResponseDto;
+import com.samnamja.deboost.api.dto.openfeign.response.GameIdResponseDto;
+import com.samnamja.deboost.api.dto.openfeign.response.SummonerDetailInfoResponseDto;
+import com.samnamja.deboost.api.dto.openfeign.response.SummonerInfoResponseDto;
+import com.samnamja.deboost.api.dto.riot.response.GameInfoDto;
+import com.samnamja.deboost.api.dto.riot.response.GameSpecificDetailInfoResponseDto;
 import com.samnamja.deboost.api.dto.riot.response.SummonerSearchResponseDto;
 import com.samnamja.deboost.api.entity.riot.AnalysisData.AnalysisData;
 import com.samnamja.deboost.api.entity.riot.AnalysisData.AnalysisDataRepository;
@@ -14,10 +19,10 @@ import com.samnamja.deboost.utils.aws.AmazonS3Uploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,21 +33,131 @@ import java.util.stream.Collectors;
 public class RiotDataService {
     @Value("${riot.api-key}")
     private String riotApiKey;
-
+    private final ObjectMapper objectMapper;
     private final RiotOpenFeignService riotOpenFeignService;
     private final ManuFactureDataService manuFactureDataService;
     private final AnalysisDataRepository analysisDataRepository;
     private final UserHistoryRepository userHistoryRepository;
     private final AmazonS3Uploader amazonS3Uploader;
 
-    @Transactional
-    public SummonerSearchResponseDto analysisSummoner(String summonerName){
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<GameSpecificDetailInfoResponseDto> response = new ArrayList<>();
-        // RIOT API 호출
+//    public SummonerSearchResponseDto get10GameData(String summonerName) {
+//        AtomicReference<SummonerSearchResponseDto> summonerSearchResponseDto = null;
+//        userHistoryRepository.findByHistoryGamerName(summonerName)
+//                .ifPresentOrElse(userHistory -> {
+//                    // 2. userHistory 에 존재하면 -> analysis data에서 10개 찾아서 데이터 구성해서 보내기
+//                    List<GameInfoDto> gameInfoDtos = new ArrayList<>();
+//                    SummonerInfoResponseDto summonerInfo = riotOpenFeignService.getSummonerEncryptedId(summonerName, riotApiKey);
+//                    List<SummonerDetailInfoResponseDto> summonerDetailInfo = riotOpenFeignService.getSummonerDetailInfo(summonerInfo.getId(), riotApiKey);
+//                    analysisDataRepository.findTop10ByUserHistory_IdOrderByCreatedAt(userHistory.getId())
+//                            .stream().map(AnalysisData::getPrimaryDataUrl).collect(Collectors.toList())
+//                            .forEach(url -> {
+//                                GameAllDetailInfoResponseDto gameDetailInfos = amazonS3Uploader.loadJsonFileAndConvertToDto(url);
+//                                gameInfoDtos.add(GameInfoDto.from(gameDetailInfos, summonerName));
+//                            });
+//                    summonerSearchResponseDto.set(SummonerSearchResponseDto.builder()
+//                            .summonerInfo(SummonerSearchResponseDto.SummonerInfo.builder()
+//                                    .summonerName(summonerInfo.getName())
+//                                    .summonerLevel(summonerInfo.getSummonerLevel())
+//                                    .summonerIconId(summonerInfo.getProfileIconId())
+//                                    .tier(summonerDetailInfo.stream()
+//                                            .filter(detailInfo -> detailInfo.getQueueType().equals("RANKED_SOLO_5x5"))
+//                                            .findFirst().orElse(SummonerDetailInfoResponseDto.builder().tier("UNRANKED").build()).getTier()).build())
+//                            .isSearchedBefore(true)
+//                            .isUpdated(userHistory.getIsSearched())
+//                            .gameInfos(gameInfoDtos).build());
+//                }, () -> {
+//                    // 1. userHistory 에 존재하는지 검색 -> 없으면 널값 보내주기
+//                    summonerSearchResponseDto.set(SummonerSearchResponseDto.builder()
+//                            .summonerInfo(SummonerSearchResponseDto.SummonerInfo.builder()
+//                                    .summonerName(summonerName)
+//                                    .summonerLevel(0)
+//                                    .summonerIconId(0)
+//                                    .tier("")
+//                                    .build())
+//                            .isSearchedBefore(false)
+//                            .isUpdated(false)
+//                            .gameInfos(null).build());
+//                });
+//        return summonerSearchResponseDto.get();
+//    }
+
+    public SummonerSearchResponseDto get10GameData(String summonerName) {
+        return userHistoryRepository.findByHistoryGamerName(summonerName)
+                .map(this::getExistingUserData)
+                .orElseGet(() -> getNewUserData(summonerName));
+    }
+
+    private SummonerSearchResponseDto getExistingUserData(UserHistory userHistory) {
+        String summonerName = userHistory.getHistoryGamerName();
         SummonerInfoResponseDto summonerInfo = riotOpenFeignService.getSummonerEncryptedId(summonerName, riotApiKey);
         List<SummonerDetailInfoResponseDto> summonerDetailInfo = riotOpenFeignService.getSummonerDetailInfo(summonerInfo.getId(), riotApiKey);
+        List<GameInfoDto> gameInfoDtos = getGameInfoDtos(userHistory, summonerName);
+
+        return SummonerSearchResponseDto.builder()
+                .summonerInfo(buildSummonerInfo(summonerInfo, summonerDetailInfo))
+                .isSearchedBefore(true)
+                .isUpdated(userHistory.getIsSearched())
+                .gameInfos(gameInfoDtos)
+                .build();
+    }
+
+    private List<GameInfoDto> getGameInfoDtos(UserHistory userHistory, String summonerName) {
+        return analysisDataRepository.findTop10ByUserHistory_IdOrderByCreatedAtDesc(userHistory.getId())
+                .stream()
+                .map(AnalysisData::getPrimaryDataUrl)
+                .map(amazonS3Uploader::loadJsonFileAndConvertToDto)
+                .map(gameDetailInfos -> GameInfoDto.from(gameDetailInfos, summonerName))
+                .collect(Collectors.toList());
+    }
+
+    private SummonerSearchResponseDto getNewUserData(String summonerName) {
+        return SummonerSearchResponseDto.builder()
+                .summonerInfo(buildEmptySummonerInfo(summonerName))
+                .isSearchedBefore(false)
+                .isUpdated(false)
+                .gameInfos(null)
+                .build();
+    }
+
+    private SummonerSearchResponseDto.SummonerInfo buildSummonerInfo(SummonerInfoResponseDto summonerInfo, List<SummonerDetailInfoResponseDto> summonerDetailInfo) {
+        String tier = summonerDetailInfo.stream()
+                .filter(detailInfo -> detailInfo.getQueueType().equals("RANKED_SOLO_5x5"))
+                .findFirst()
+                .map(SummonerDetailInfoResponseDto::getTier)
+                .orElse("UNRANKED");
+
+        return SummonerSearchResponseDto.SummonerInfo.builder()
+                .summonerName(summonerInfo.getName())
+                .summonerLevel(summonerInfo.getSummonerLevel())
+                .summonerIconId(summonerInfo.getProfileIconId())
+                .tier(tier)
+                .build();
+    }
+
+    private SummonerSearchResponseDto.SummonerInfo buildEmptySummonerInfo(String summonerName) {
+        return SummonerSearchResponseDto.SummonerInfo.builder()
+                .summonerName(summonerName)
+                .summonerLevel(0)
+                .summonerIconId(0)
+                .tier("")
+                .build();
+    }
+
+
+    public GameSpecificDetailInfoResponseDto getDetailGameData(String gameId, String summonerName) {
+        UserHistory userHistory = userHistoryRepository.findByHistoryGamerName(summonerName)
+                .orElseThrow(() -> CustomException.builder().httpStatus(HttpStatus.NOT_FOUND).message("해당 소환사의 검색 기록이 없습니다. summonerName=" + summonerName).build());
+        String primaryDataUrl = analysisDataRepository.findAnalysisDataByGameIdAndUserHistory_Id(gameId, userHistory.getId()).getPrimaryDataUrl();
+        GameAllDetailInfoResponseDto gameAllDetailInfoResponseDto = amazonS3Uploader.loadJsonFileAndConvertToDto(primaryDataUrl);
+        return GameSpecificDetailInfoResponseDto.from(gameAllDetailInfoResponseDto, summonerName);
+    }
+
+    @Transactional
+    public void updateGameData(String summonerName){
+        // RIOT API 호출
+        SummonerInfoResponseDto summonerInfo = riotOpenFeignService.getSummonerEncryptedId(summonerName, riotApiKey);
         List<GameIdResponseDto> gameIds = riotOpenFeignService.getGameIds(summonerInfo.getPuuid(), riotApiKey);
+        gameIds.sort(Comparator.comparing(GameIdResponseDto::getGameId));
 
         UserHistory userHistory = userHistoryRepository.findByHistoryGamerName(summonerName)
                 .map(existingUserHistory -> {
@@ -59,16 +174,10 @@ public class RiotDataService {
 
         // DB에 저장된 데이터와 비교 : API 로 불러온 gameId 중에 DB에 저장된 gameId에 해당하는 것들을 호출
         List<AnalysisData> existsAnalysisData = analysisDataRepository.findAnalysisDataBySummonerNameAndGameIds(summonerName, gameIds);
-        // 이미 json 파일이 존재하는 경우 -> s3에서 json 불러와서 GameSpecificDetailInfoResponseDto 으로 load
-        existsAnalysisData.stream().map(AnalysisData::getPrimaryDataUrl).collect(Collectors.toList()).stream()
-                .forEach(url -> {
-                    GameAllDetailInfoResponseDto gameDetailInfos = amazonS3Uploader.loadJsonFileAndConvertToDto(url);
-                    response.add(GameSpecificDetailInfoResponseDto.from(gameDetailInfos, summonerName));
-                });
 
         // 새로 추가된 게임이 있는 경우 -> API 호출해서 GameSpecificDetailInfoResponseDto 으로 load
-        List<GameAllDetailInfoResponseDto> newSearchGamesDetailInfos = gameIds.stream().map(GameIdResponseDto::getGameId).collect(Collectors.toSet()).stream()
-                .filter(gameId -> !existsAnalysisData.stream().map(AnalysisData::getGameId).collect(Collectors.toSet()).contains(gameId))
+        List<GameAllDetailInfoResponseDto> newSearchGamesDetailInfos = gameIds.stream().map(GameIdResponseDto::getGameId).collect(Collectors.toList()).stream()
+                .filter(gameId -> !existsAnalysisData.stream().map(AnalysisData::getGameId).collect(Collectors.toList()).contains(gameId))
                 .collect(Collectors.toList()).stream()
                 .map(gameId -> riotOpenFeignService.getGameDetailInfos(gameId, riotApiKey)).collect(Collectors.toList());
 
@@ -88,27 +197,6 @@ public class RiotDataService {
                     .build();
             analysisDataRepository.save(analysisData);
         });
-
-        List<GameSpecificDetailInfoResponseDto> newGameSpecificDetailInfo = newSearchGamesDetailInfos.stream()
-                .map(gameDetailInfos -> GameSpecificDetailInfoResponseDto.from(gameDetailInfos, summonerName))
-                .collect(Collectors.toList());
-
-        response.addAll(newGameSpecificDetailInfo);
-        response.sort(Comparator.comparing(GameSpecificDetailInfoResponseDto::getGameId).reversed());
-
-        return SummonerSearchResponseDto.builder()
-                .summonerInfo(
-                        SummonerSearchResponseDto.SummonerInfo.builder()
-                                .summonerName(summonerInfo.getName())
-                                .summonerLevel(summonerInfo.getSummonerLevel())
-                                .summonerIconId(summonerInfo.getProfileIconId())
-                                .tier(summonerDetailInfo.stream()
-                                        .filter(detailInfo -> detailInfo.getQueueType().equals("RANKED_SOLO_5x5"))
-                                        .findFirst().orElse(SummonerDetailInfoResponseDto.builder().tier("UNRANKED").build()).getTier())
-                                .build()
-                            )
-                .gameInfos(response)
-                .build();
     }
 
 }
