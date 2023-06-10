d6736b397cecc40dbfaf02860944778b4157bba3
99winnmin <ojysep9987@gmail.com>
2023-06-10 21:00:42 +0900

    [Feat] 동화 리스트, 상세정보 받기 API 구현 완료

--- src/main/java/com/samnamja/deboost/api/service/fairy/diary/FairyDiaryService.java
+++ src/main/java/com/samnamja/deboost/api/service/fairy/diary/FairyDiaryService.java
@@ -4,13 +4,22 @@ import com.samnamja.deboost.api.dto.fairy.request.DiaryMakeRequestDto;
 import com.samnamja.deboost.api.dto.fairy.request.FairyRequestDto;
 import com.samnamja.deboost.api.dto.fairy.request.MessageRequestDto;
 import com.samnamja.deboost.api.dto.fairy.response.ChatCompletionDto;
+import com.samnamja.deboost.api.dto.fairy.response.FairyTaleCoverResponseDto;
+import com.samnamja.deboost.api.dto.fairy.response.FairyTaleDetailResponseDto;
+import com.samnamja.deboost.api.entity.fairy.diary.Diary;
+import com.samnamja.deboost.api.entity.fairy.diary.DiaryRepository;
+import com.samnamja.deboost.api.entity.user.detail.UserAccount;
 import com.samnamja.deboost.api.service.openfeign.FairyOpenFeignClient;
+import com.samnamja.deboost.exception.custom.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +31,8 @@ public class FairyDiaryService {
 
     @Value("${chatGPT.model}")
     private String model;
+
+    private final DiaryRepository diaryRepository;
 
     public ChatCompletionDto getFairyContent(DiaryMakeRequestDto diaryMakeRequestDto) {
         List<MessageRequestDto> messageList = new ArrayList<>();
@@ -36,5 +47,15 @@ public class FairyDiaryService {
                 .messages(messageList)
                 .build();
         return fairyOpenFeignClient.getFairyContent("Bearer " + openaiKey, fairyRequestDto);
+    }
+
+    public List<FairyTaleCoverResponseDto> getMyFairyTales(UserAccount userAccount, Long cursor, Pageable pageable) {
+        List<Diary> diaryList = diaryRepository.findAllByDeviceIdWithPagingOrderByCreatedAt(userAccount.getUserId(), cursor, pageable);
+        return diaryList.stream().map(FairyTaleCoverResponseDto::of).collect(Collectors.toList());
+    }
+
+    public FairyTaleDetailResponseDto getFairyTaleDetailInfo(UserAccount userAccount, Long diaryId) {
+        Diary diary = diaryRepository.findById(diaryId).orElseThrow(() -> CustomException.builder().httpStatus(HttpStatus.NOT_FOUND).message("해당 동화가 존재하지 않습니다.").build());
+        return FairyTaleDetailResponseDto.of(diary);
     }
 }
