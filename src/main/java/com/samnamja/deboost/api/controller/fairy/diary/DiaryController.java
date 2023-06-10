bfffaed101992fac4f292f354873721bb00ae242
99winnmin <ojysep9987@gmail.com>
2023-06-10 20:59:50 +0900

    [Feat] 동화 리스트, 상세 정보 받기 API 구현

--- src/main/java/com/samnamja/deboost/api/controller/fairy/diary/DiaryController.java
+++ src/main/java/com/samnamja/deboost/api/controller/fairy/diary/DiaryController.java
@@ -2,12 +2,20 @@ package com.samnamja.deboost.api.controller.fairy.diary;
 
 import com.samnamja.deboost.api.dto.fairy.request.DiaryMakeRequestDto;
 import com.samnamja.deboost.api.dto.fairy.response.ChatCompletionDto;
+import com.samnamja.deboost.api.dto.fairy.response.FairyTaleCoverResponseDto;
+import com.samnamja.deboost.api.dto.fairy.response.FairyTaleDetailResponseDto;
+import com.samnamja.deboost.api.entity.user.detail.UserAccount;
 import com.samnamja.deboost.api.service.fairy.diary.FairyDiaryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +23,37 @@ public class DiaryController {
     private final FairyDiaryService fairyDiaryService;
 
     @PostMapping("/init/diary")
-    public ResponseEntity<ChatCompletionDto> getFairyContent(@RequestBody DiaryMakeRequestDto diaryMakeRequestDto) {
-        return ResponseEntity.ok().body(fairyDiaryService.getFairyContent(diaryMakeRequestDto));
+    public ResponseEntity<List<String>> getFairyContent
+            (
+                    @AuthenticationPrincipal UserAccount userAccount,
+                    @RequestBody DiaryMakeRequestDto diaryMakeRequestDto
+            )
+    {
+        ChatCompletionDto content = fairyDiaryService.getFairyContent(diaryMakeRequestDto);
+        return ResponseEntity.ok().body(Arrays.asList(content.getChoices().get(0).getMessage().getContent().split("\\n\\n")));
+    }
+
+    @GetMapping("/diary/all")
+    public ResponseEntity<List<FairyTaleCoverResponseDto>> getMyFairyTales
+            (
+                    @AuthenticationPrincipal UserAccount userAccount,
+                    @Nullable @RequestParam Long cursor,
+                    @PageableDefault(size = 4) Pageable pageable
+            )
+    {
+        List<FairyTaleCoverResponseDto> myFairyTaleCovers = fairyDiaryService.getMyFairyTales(userAccount, cursor, pageable);
+        return ResponseEntity.ok().body(myFairyTaleCovers);
+    }
+
+    @GetMapping("/diary/{diaryId}")
+    public ResponseEntity<FairyTaleDetailResponseDto> getFairyTale
+            (
+                    @AuthenticationPrincipal UserAccount userAccount,
+                    @PathVariable Long diaryId
+            )
+    {
+        FairyTaleDetailResponseDto fairyTaleDetailResponseDto = fairyDiaryService.getFairyTaleDetailInfo(userAccount, diaryId);
+        return ResponseEntity.ok().body(fairyTaleDetailResponseDto);
     }
 
 }
