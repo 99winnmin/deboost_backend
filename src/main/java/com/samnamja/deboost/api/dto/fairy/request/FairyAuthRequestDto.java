ea37a0401a4bdb97bb22160f8b3774df4d79a855
99winnmin <ojysep9987@gmail.com>
2023-06-10 16:50:38 +0900

    [Feat] fiary 기기 등록 Req DTO

---
+++ src/main/java/com/samnamja/deboost/api/dto/fairy/request/FairyAuthRequestDto.java
@@ -0,0 +1,10 @@
+package com.samnamja.deboost.api.dto.fairy.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class FairyAuthRequestDto {
+    private String deviceName;
+}
