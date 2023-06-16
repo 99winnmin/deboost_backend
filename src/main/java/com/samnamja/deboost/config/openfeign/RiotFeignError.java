1c45988ffd7892a71878bff339919c3d157eb68f
99winnmin <ojysep9987@gmail.com>
2023-06-16 18:52:01 +0900

    [Feat] 라이엇 API 연동 ERROR 로직 추가

--- src/main/java/com/samnamja/deboost/config/openfeign/RiotFeignError.java
+++ src/main/java/com/samnamja/deboost/config/openfeign/RiotFeignError.java
@@ -2,15 +2,27 @@ package com.samnamja.deboost.config.openfeign;
 
 import com.samnamja.deboost.exception.custom.CustomException;
 import feign.Response;
+import feign.Util;
 import feign.codec.ErrorDecoder;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 
+import java.nio.charset.StandardCharsets;
+
 public class RiotFeignError implements ErrorDecoder {
+    @SneakyThrows
     @Override
     public Exception decode(String methodKey, Response response) {
         switch (response.status()) {
             case 404:
-                return CustomException.builder().httpStatus(HttpStatus.NOT_FOUND).message("라이엇 API 오류 발생").build();
+                byte[] responseBodyBytes = Util.toByteArray(response.body().asInputStream());
+                String responseBody = new String(responseBodyBytes, StandardCharsets.UTF_8);
+
+                if (responseBody.contains("Data not found - summoner not found")) {
+                    return CustomException.builder().httpStatus(HttpStatus.NOT_FOUND).message("소환사를 찾을 수 없습니다.").build();
+                } else {
+                    return CustomException.builder().httpStatus(HttpStatus.NOT_FOUND).message("라이엇 API 오류 발생").build();
+                }
         }
         return null;
     }
